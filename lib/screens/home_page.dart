af086d4157ee9a54489f599240928f8905723587
Rabia Okatan <rabiaro26@gmail.com>
2021-04-05 17:39:02 +0300

    slider look added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,19 +7,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _current = 0;
+  List imgList = [
+    'https://images.stockx.com/images/Anti-Social-Social-Club-Kkoch-Hoodie-Black.jpg?fit=fill&bg=FFFFFF&w=300&h=214&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1611841873',
+    'http://www.bambistore.com.tr/Uploads/UrunResimleri/buyuk/beyazsiyah-kadin-sneakers-l0512438309-e2fe.jpg',
+    'https://cdn-images.farfetch-contents.com/15/60/02/30/15600230_29200357_300.jpg',
+    'https://ae01.alicdn.com/kf/HTB1wq10RkPoK1RjSZKbq6x1IXXaJ/Bahar-fener-kollu-i-ek-nak-rg-kazak-h-rka-sonbahar-tiki-tarz-Mori-k-z.jpg_q50.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.only(top: 20, left: 20),
+            padding: const EdgeInsets.only(
+              top: 20, /*eft: 20*/
+            ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 //header
                 Padding(
-                  padding: const EdgeInsets.only(right: 20),
+                  padding: const EdgeInsets.only(right: 20, left: 20),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -66,121 +77,194 @@ class _HomePageState extends State<HomePage> {
                 ),
                 //divider
                 Container(
-                  margin: EdgeInsets.only(top: 20, bottom: 20, right: 20),
+                  margin:
+                      EdgeInsets.only(top: 20, bottom: 20, right: 20, left: 20),
                   height: 2,
                   width: double.infinity,
                   color: Colors.grey[350],
                 ),
                 //middle field
-                Stack(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(right: 20),
-                      height: 375,
-                      width: double.infinity,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 20),
-                      width: double.infinity,
-                      height: 355,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(20),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height:490,
+                    initialPage: 0,
+                    autoPlay: true,
+                    autoPlayAnimationDuration: Duration(seconds: 2),
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _current = index;
+                      });
+                    },
+                  ),
+                  items: imgList.map((imgUrl) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Stack(
+                                children: [
+                                  Container(
+                                    height:375,
+                                  ),
+                                  Container(
+                                    margin:
+                                        EdgeInsets.symmetric(horizontal: 10),
+                                    height: 355,
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                      color: Colors.blue,
+                                      borderRadius: BorderRadius.circular(30),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.5),
+                                          spreadRadius: 2,
+                                          blurRadius: 5,
+                                        ),
+                                      ],
+                                    ),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: Image.network(
+                                        imgUrl,
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        right: 34, top: 14),
+                                    child: Align(
+                                      alignment: Alignment.topRight,
+                                      child: Icon(Icons.favorite_border,
+                                          color: Colors.black),
+                                    ),
+                                  ),
+                                  //shopping icon
+                                  Positioned(
+                                    top:320,
+                                    right:14,
+                                    child: Container(
+                                      margin:
+                                          EdgeInsets.only(right: 34, bottom: 5),
+                                      padding: EdgeInsets.all(6),
+                                      height: 50,
+                                      width: 50,
+                                      decoration: BoxDecoration(
+                                        color: Color(0xff2F69F8),
+                                        borderRadius: BorderRadius.circular(35),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.grey.withOpacity(0.5),
+                                            spreadRadius: 3,
+                                            blurRadius: 5,
+                                          ),
+                                        ],
+                                      ),
+                                      alignment: Alignment.center,
+                                      child: Icon(Icons.shopping_bag,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              // middle field footer
+                              Container(
+                                margin: EdgeInsets.only(left: 20),
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 5, vertical: 1),
+                                decoration: BoxDecoration(
+                                  color: Color(0xff7750EA),
+                                  borderRadius: BorderRadius.circular(2),
+                                ),
+                                child: Text(
+                                  'NEW',
+                                  style: TextStyle(
+                                      fontSize: 8, color: Colors.white),
+                                ),
+                              ),
+                              SizedBox(height:5),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(right: 20, left: 20),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        'Anti Social Club Hoodie',
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w700),
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                    SizedBox(width:5),
+                                    Text(
+                                      '\$60',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(right: 20, left: 20),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Long 3/4 sleevs, sweartshirt',
+                                      style: TextStyle(
+                                          color: Color(0xff6A6A6A),
+                                          fontSize: 12),
+                                    ),
+                                    SizedBox(width:5),
+                                    Text(
+                                      'VAT included',
+                                      style: TextStyle(
+                                          color: Color(0xff6A6A6A),
+                                          fontSize: 12),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: imgList.map((url) {
+                                  int index = imgList.indexOf(url);
+                                  return Container(
+                                    width: 8.0,
+                                    height: 8.0,
+                                    margin: EdgeInsets.symmetric(
+                                        vertical: 10.0, horizontal: 2.0),
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: _current == index
+                                          ? Color.fromRGBO(0, 0, 0, 0.9)
+                                          : Color.fromRGBO(0, 0, 0, 0.4),
+                                    ),
+                                  );
+                                }).toList(),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 34, top: 14),
-                      child: Align(
-                        alignment: Alignment.topRight,
-                        child: Icon(Icons.favorite_border, color: Colors.white),
-                      ),
-                    ),
-                    //shopping icon
-                    Positioned(
-                      left: 250,
-                      top: 320,
-                      child: Container(
-                        padding: EdgeInsets.all(6),
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                          color: Color(0xff2F69F8),
-                          borderRadius: BorderRadius.circular(35),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset:
-                                  Offset(0, 3), // changes position of shadow
-                            ),
-                          ],
-                        ),
-                        alignment: Alignment.center,
-                        child: Icon(Icons.shopping_bag, color: Colors.white),
-                      ),
-                    ),
-                  ],
+                        );
+                      },
+                    );
+                  }).toList(),
                 ),
-                // middle field footer
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 5, vertical: 1),
-                  decoration: BoxDecoration(
-                    color: Color(0xff7750EA),
-                    borderRadius: BorderRadius.circular(2),
-                  ),
-                  child: Text(
-                    'NEW',
-                    style: TextStyle(fontSize: 8, color: Colors.white),
-                  ),
-                ),
-                SizedBox(height: 10),
+
+                //
                 Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: Text(
-                          'Anti Social Club Hoodie',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w700),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                      Text(
-                        '\$60',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w700),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Long 3/4 sleevs, sweartshirt',
-                        style:
-                            TextStyle(color: Color(0xff6A6A6A), fontSize: 12),
-                      ),
-                      Text(
-                        'VAT included',
-                        style:
-                            TextStyle(color: Color(0xff6A6A6A), fontSize: 12),
-                      ),
-                    ],
-                  ),
+                  padding: const EdgeInsets.only(left:20),
+                  child: Text('You may like', style: TextStyle(fontSize: 20)),
                 ),
               ],
             ),
